68f7433a96eff20e7f46dff66e772efa07b3bb98
Connor Keenum <46159829+Nellak2017@users.noreply.github.com>
2019-01-12 18:31:09 +0300

    Created functions up to p03
    Added fib and maxPF

---
+++ Solution-Code/Euler_Lib.py
@@ -0,0 +1,28 @@
+#This file contains all of the library functions used in my project euler solutions
+#Keep this file in Alphabetical Order
+#Contains:
+#fib,maxPF
+
+import math
+
+def fib(n):
+    # Returns fibonnacci(n), where fib(n) = fib(n-1)+fib(n-2)
+    a,b = 0,1
+    for x in range(0,n):
+        a, b = b, a + b
+    return a
+
+def maxPF(n):
+    # Returns the max prime factor of a number
+    maxPrime = -1
+    while n%2 == 0:
+        maxPrime = 2
+        n /= 2
+    for x in range(3, int(math.sqrt(n)) + 1, 2):
+        while n % x == 0:
+            maxPrime = x
+            n = n / x
+    if n > 2:
+        maxPrime = n    
+    return int(maxPrime)    
+    
