a30b8eae2d6ccfcea75c1669bec5c65f0e0c07d1
Connor Keenum <46159829+Nellak2017@users.noreply.github.com>
2019-01-12 19:05:38 +0300

    Updated to p05
    Added isprime and fact functions, and put in organization for easier alphabetical ordering.

--- Solution-Code/Euler_Lib.py
+++ Solution-Code/Euler_Lib.py
@@ -1,9 +1,20 @@
 #This file contains all of the library functions used in my project euler solutions
 #Keep this file in Alphabetical Order
 #Contains:
-#fib,maxPF
+#fact,fib,is_palindrome,isprime,maxPF
 
-import math
+import math,random
+
+# F
+def fact(n):
+    # Returns the factorial of n, where factorial is n*(n-1)*(n-2)...*(1)
+    if n >= 0:
+        product = 1
+        for i in range(n):
+            product *= i+1
+    else:
+        raise ValueError('factorial is not defined for negative numbers')
+    return product
 
 def fib(n):
     # Returns fibonnacci(n), where fib(n) = fib(n-1)+fib(n-2)
@@ -11,13 +22,43 @@ def fib(n):
     for x in range(0,n):
         a, b = b, a + b
     return a
-
+# I
 def is_palindrome(s):
     # Returns True if a string is a palindrome, False otherwise. 
     # A Palindrome is a string that is the same forwards and backwards.
     digits = list(str(s))
     return digits[::-1] == digits[:]
 
+def isprime(n, precision=7):
+    _smallprimeset = 1000000
+    # http://en.wikipedia.org/wiki/Miller-Rabin_primality_test
+    # Algorithm_and_running_time
+    if n < 1:
+        raise ValueError("Out of bounds, first argument must be > 0")
+    elif n <= 3:
+        return n >= 2
+    elif n % 2 == 0:
+        return False
+        d = n - 1
+    s = 0
+    while d % 2 == 0:
+        d //= 2
+        s += 1
+
+    for repeat in range(precision):
+        a = random.randrange(2, n - 2)
+        x = pow(a, d, n)
+
+        if x == 1 or x == n - 1: continue
+
+        for r in range(s - 1):
+            x = pow(x, 2, n)
+            if x == 1: return False
+            if x == n - 1: break
+        else: return False
+
+    return True
+# M
 def maxPF(n):
     # Returns the max prime factor of a number
     maxPrime = -1
