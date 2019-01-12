d506e3b1bfb38d29c94322d2d7c33accb7255893
Connor Keenum <46159829+Nellak2017@users.noreply.github.com>
2019-01-12 19:40:42 +0300

    Created p07.py
    Uses nth_prime library function.

---
+++ Solution-Code/p07.py
@@ -0,0 +1,9 @@
+#Project Euler problem 7
+#10 001st prime
+import math
+from Solution-Code import Euler_Lib as E
+
+print(E.nth_prime(10001))
+'''
+The Library function "nth_prime" returns the nth prime, we only need the 10001st, so this is trivial.
+'''
