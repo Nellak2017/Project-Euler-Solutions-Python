c09108be5bfddd384702d0e8ba12731b418d880d
Connor Keenum <Nellakprime@gmail.com>
2021-07-09 10:46:36 +0300

    Created p31.py

---
+++ Solution-Code/p31.py
@@ -0,0 +1,7 @@
+# Euler 31
+# Aug 26 2018
+# Ways to partition coins with known denomination sizes
+# Solve using dynamic programming
+import Euler_Lib as f
+
+print(f.partition([1,2,5,10,20,50,100,200],200))
