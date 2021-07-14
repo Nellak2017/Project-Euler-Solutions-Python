a26bbebc3b6050a0b4ae77fb6e56be43bc19c700
Connor Keenum <Nellakprime@gmail.com>
2021-07-14 20:13:56 +0300

    Created p36.py

---
+++ Solution-Code/p36.py
@@ -0,0 +1,7 @@
+# Euler 36
+# Aug 28 2018
+# http://oeis.org/A007632
+
+l = [0, 1, 3, 5, 7, 9, 33, 99, 313, 585, 717, 7447, 9009, 15351, 32223, 39993, 53235, 53835, 73737, 58558]
+#https://github.com/luckytoilet/projecteuler-solutions/blob/master/Solutions.md
+print('872187')
